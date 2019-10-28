fe99d0a2a6d1229fc83a75452d57dad7d68b9f1e
Adji Fatou F Diagne (CENSUS/ERD FED) <adji.f.diagne@census.gov>
2019-10-28 11:32:56 -0400

    Course Project 1

---
+++ plot1.R
@@ -0,0 +1,14 @@
+setwd("H:/Coursera/Exploratory Data Analysis Project 1")
+
+dataset <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                    nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data1 <- subset(dataset, Date %in% c("1/2/2007","2/2/2007"))
+data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
+
+## Plot 1
+hist(data1$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+## Saving file to png
+png("plot1.png", width=480, height=480)
+dev.off()
