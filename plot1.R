867ddf9c3bc3021459196fcef6d3d797b92cfa45
AdjiFatouDiagne <53020502+AdjiFatouDiagne@users.noreply.github.com>
2019-11-08 08:17:42 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,5 +10,5 @@ hist(data1$Global_active_power, main="Global Active Power",
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ## Saving file to png
-png("plot1.png", width=480, height=480)
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
